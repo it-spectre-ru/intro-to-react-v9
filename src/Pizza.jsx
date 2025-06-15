16685f7132217a68a42525ef9beeb730ae18eca8
Note <it.spectre.ru@gmail.com>
2025-06-15 23:53:59 +0500

    Creating the Order Component

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,8 +1,4 @@
-let counter = 0;
-
 const Pizza = (props) => {
-  counter = Date.now();
-
   return (
     <div
       className="pizza"
@@ -10,9 +6,7 @@ const Pizza = (props) => {
         console.log("hi00");
       }}
     >
-      <h1>
-        {props.name} {counter}
-      </h1>
+      <h1>{props.name}</h1>
       <p>{props.description}</p>
       <img src={props.image} alt={props.name} />
     </div>
