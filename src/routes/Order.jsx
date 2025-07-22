e5a9ef6677bbf5876dbcea885f1818ff5918bc3d
Note <it.spectre.ru@gmail.com>
2025-07-23 00:17:04 +0500

    routing

--- src/routes/Order.jsx
+++ src/routes/Order.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useContext } from "react";
-import { CartContext } from "./contexts";
-import Cart from "./Cart";
-import Pizza from "./Pizza";
+import { CartContext } from "../contexts";
+import Cart from "../Cart";
+import Pizza from "../Pizza";
 
 // feel free to change en-US / USD to your locale
 const intl = new Intl.NumberFormat("en-US", {
